e8953bb386dfe2ca1c1208a5c6b9583aadd598f7
artificialsoph <s@soph.info>
2019-09-12 11:00:11 -0700

    replace echo with printf?

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -lx
 mkdir ~/.ssh
-echo "$INPUT_SSH_KEY" >~/.ssh/id_rsa
+printf "$INPUT_SSH_KEY" >~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
 git clone git@github.com:$GITHUB_REPOSITORY.git instructor_repo
