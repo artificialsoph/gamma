c433758b5bebe650adc98f3476b6f59f967b2aed
artificialsoph <s@soph.info>
2019-09-12 11:04:27 -0700

    fix printf?

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -lx
 mkdir ~/.ssh
-printf "$INPUT_SSH_KEY" >~/.ssh/id_rsa
+printf "%s" "$var" >~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
 git clone git@github.com:$GITHUB_REPOSITORY.git instructor_repo
