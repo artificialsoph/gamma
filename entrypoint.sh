82a404d02e252d7a7627389f1321ff15ead560a0
artificialsoph <s@soph.info>
2019-09-12 10:43:43 -0700

    remove bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,6 @@ mkdir ~/.ssh
 echo $INPUT_SSH_KEY >~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-/bin/bash
-
 git clone git@github.com:$GITHUB_REPOSITORY.git instructor_repo
 cd instructor_repo
 gamma generate
